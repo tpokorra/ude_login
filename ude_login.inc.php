b8783786d6bedf755c4c916cda08da26ccfa88fc
Timotheus Pokorra <tp@tbits.net>
2016-10-07 11:29:51 +0200

    rename the text file with the permissions for the users

--- ude_login.inc.php
+++ ude_login.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 $config['ude_use_grep'] = true;
-$config['ude_login_db'] = '/etc/roundcubemail/users.txt';
+$config['ude_login_db'] = '/etc/roundcubemail/enable_disable_per_user.txt';
 #$config['ude_default_plugins'] = array('acl', 'managesieve');
 
 ?>
